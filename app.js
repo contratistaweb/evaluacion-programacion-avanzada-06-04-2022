44a9bb2f932e320362e165f807a45a6472c06d60
Luillin Escobar <luillin.escobar@hotmail.com>
2022-04-06 21:59:01 -0500

    se agrega pregunta con seleccion multiple

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const inquirer = require('inquirer');
-inquirer.prompt([
-    {
-        type: 'input',
-        name: 'name',
-        message: "What's your full name",
-    }, {
-        type: 'input',
-        name: 'cellphone',
-        message: "What's your cellphone number",
-    }, {
-        type: 'input',
-        name: 'address',
-        message: "What's your address",
-    }
-]).then(answers => console.log(answers));
+// inquirer.prompt([
+//     {
+//         type: 'input',
+//         name: 'name',
+//         message: "What's your full name",
+//     }, {
+//         type: 'input',
+//         name: 'cellphone',
+//         message: "What's your cellphone number",
+//     }, {
+//         type: 'input',
+//         name: 'address',
+//         message: "What's your address",
+//     }
+// ]).then(answers => console.log(answers));
+
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'favorite_color',
+            message: 'What is your favorite color?',
+            choices: ['Yellow', 'Blue', 'Red', 'White', 'Black'],
+        },
+    ])
+    .then((answers) => {
+        console.log(JSON.stringify(answers, null, '  '));
+    });
